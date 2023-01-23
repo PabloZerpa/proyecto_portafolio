6f36a7d429623d5faea4fa248ede33dc444ab689
Pablo <prueba@test.com>
2023-01-23 06:23:11 -0400

    Deploy

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -1,6 +1,8 @@
 
 import axios from 'axios';
-const baseUrl = "http://localhost:3001/api/";
+//const baseUrl = "http://localhost:3001/api/";
+const baseUrl = "https://proyecto-portafolio-server.onrender.com/api/"
+
 
 class Autorizacion {
 
